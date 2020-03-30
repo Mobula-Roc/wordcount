9ceb558849d2374d038a2835884446c2f9b046f1
Mobula-Roc <1101553700@qq.com>
2020-03-31 01:23:31 +0800

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Program Files\JetBrains\PyCharm 2018.3.3\PycharmProjects\wordcount\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
